72778272078bbe97c02328985189a60ece003b48
laurencetroyv-asus <laurencetroyv@gmail.com>
2023-05-21 00:49:54 +0800

    fix: change navigation import to component location

--- App.js
+++ App.js
@@ -2,7 +2,7 @@ import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { Provider } from 'react-native-paper';
 import AuthenticationProvider from './src/services/AuthenticationProvider';
-import Navigation from './src/infrastructure/Navigation';
+import Navigation from './src/infrastructure/navigation/Navigation';
 
 export default function App() {
   return (
