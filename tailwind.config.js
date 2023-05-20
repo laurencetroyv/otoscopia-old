9efd87baa9d4b4f64939054b50f75826c640bf63
laurencetroyv-asus <laurencetroyv@gmail.com>
2023-05-20 20:50:18 +0800

    chore: initializing tailwindcss

---
+++ tailwind.config.js
@@ -0,0 +1,8 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['/App.js', '/src/feature/**/*.{js,jsx}', '/src/components/*.{js,jsx}'],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
