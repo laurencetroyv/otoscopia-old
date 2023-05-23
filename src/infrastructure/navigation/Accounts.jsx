38126809b6751401a86f7030ab059d1db14e79da
laurencetroyv-hp <laurencetroyv@gmail.com>
2023-05-23 18:51:37 +0800

    fix: change SignIn import Accounts to accounts

--- src/infrastructure/navigation/Accounts.jsx
+++ src/infrastructure/navigation/Accounts.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import SignIn from '../../features/Accounts/Sign-in';
+import SignIn from '../../features/accounts/Sign-in';
 
 const Stack = createNativeStackNavigator();
 
