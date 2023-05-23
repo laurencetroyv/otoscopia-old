4ba02d89416a06b654627b8ad83c36922978b5e8
laurencetroyv-asus <laurencetroyv@gmail.com>
2023-05-23 14:35:16 +0800

    refractor: format document for code readability

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,12 @@
 /** @type {import('tailwindcss').Config} */
 
-const colors = require('./src/infrastructure/themes/colors')
+const colors = require('./src/infrastructure/themes/colors');
 
 module.exports = {
-  content: ['./App.js', './src/feature/**/*.{js,jsx}', './src/components/*.jsx'],
+  content: ['./App.js', './src/features/**/*.{js,jsx}', './src/components/*.jsx'],
   theme: {
     extend: {
-      colors
+      colors,
     },
   },
   plugins: [],
