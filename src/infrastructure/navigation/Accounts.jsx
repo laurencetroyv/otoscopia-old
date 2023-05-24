e8ee85205760dd6475eb2e9d745d3d5aea38daca
laurencetroyv-asus <laurencetroyv@gmail.com>
2023-05-24 14:01:29 +0800

    feat: Add loading screen

--- src/infrastructure/navigation/Accounts.jsx
+++ src/infrastructure/navigation/Accounts.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import SignIn from '../../features/accounts/Sign-in';
 import SignUp from '../../features/accounts/Sign-up';
+import LoadingScreen from '../../components/Loading-Screen';
 
 const Stack = createNativeStackNavigator();
 
@@ -13,6 +14,10 @@ function SignUpScreen() {
   return <SignUp />;
 }
 
+function LoadingScreenFunction() {
+  return <LoadingScreen />;
+}
+
 export default function Accounts() {
   const screenOptions = {
     headerShown: false,
@@ -22,6 +27,7 @@ export default function Accounts() {
     <Stack.Navigator screenOptions={screenOptions}>
       <Stack.Screen name="Sign In" component={SignInScreen} />
       <Stack.Screen name="Sign Up" component={SignUpScreen} />
+      <Stack.Screen name="Loading" component={LoadingScreenFunction} />
     </Stack.Navigator>
   );
 }
