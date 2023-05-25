3727fc86f9158179c66bafb9dc3221b52db2cf03
laurencetroyv-hp <laurencetroyv@gmail.com>
2023-05-25 17:27:16 +0800

    feat: Pressable Text now have center props that will center text

--- src/components/Pressable-Text.jsx
+++ src/components/Pressable-Text.jsx
@@ -3,10 +3,10 @@ import { TouchableOpacity } from 'react-native';
 import PropTypes from 'prop-types';
 import { Text } from 'react-native-paper';
 
-export default function PressableText({ text, strongText, navigate }) {
+export default function PressableText({ text, strongText, onPress, center }) {
   return (
-    <TouchableOpacity onPress={navigate}>
-      <Text className="text-center">
+    <TouchableOpacity onPress={onPress}>
+      <Text className={`${center ? "text-center" : null}`}>
         <Text>{text}</Text>
         <Text className="font-bold">{strongText}</Text>
       </Text>
@@ -17,5 +17,6 @@ export default function PressableText({ text, strongText, navigate }) {
 PressableText.propTypes = {
   text: PropTypes.node.isRequired,
   strongText: PropTypes.node.isRequired,
-  navigate: PropTypes.func.isRequired,
+  onPress: PropTypes.func.isRequired,
+  center: PropTypes.bool.isRequired
 };
