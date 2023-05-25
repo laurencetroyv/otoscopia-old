183c450deb1a4b0561014f6d4db1159495d6728d
laurencetroyv-hp <laurencetroyv@gmail.com>
2023-05-25 16:55:44 +0800

    feat: Add EmailInput component

---
+++ src/features/accounts/components/EmailInput.jsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { TextInput } from 'react-native-paper';
+import StyledTextInput from '../../../components/Styled-TextInput';
+
+export default function EmailInput({ onChangeText, onEndEditing, showHelper }) {
+  return (
+    <StyledTextInput
+      label="Email Address"
+      autoCapitalize="words"
+      autoComplete="email"
+      inputMode="email"
+      keyboardType="email-address"
+      onChangeText={onChangeText}
+      onEndEditing={onEndEditing}
+      placeholder="@deped.edu.ph"
+      textContentType="emailAddress"
+      right={<TextInput.Icon icon="email" forceTextInputFocus={false} />}
+      showHelper={showHelper}
+      helperMessage="Invalid email address"
+    />
+  );
+}
+
+EmailInput.propTypes = {
+  onChangeText: PropTypes.func.isRequired,
+  onEndEditing: PropTypes.func.isRequired,
+  showHelper: PropTypes.bool.isRequired,
+};
