4b1d0a37764e0bef9c00d20609698ea8ef70b382
laurencetroyv-hp <laurencetroyv@gmail.com>
2023-05-25 17:26:17 +0800

    fix: ThirdTitle now have no dot at the end

--- src/features/accounts/components/Header.jsx
+++ src/features/accounts/components/Header.jsx
@@ -7,7 +7,7 @@ export default function Header({ title, secondTitle, thirdTitle, subtitle }) {
     <>
       <Text className="font-bold text-3xl text-primary">{title}</Text>
       <Text className="font-bold text-3xl">{secondTitle}.</Text>
-      {!!thirdTitle && <Text className="font-bold text-3xl">{thirdTitle}.</Text>}
+      {!!thirdTitle && <Text className="font-bold text-3xl">{thirdTitle}</Text>}
       <Text className="text-sm w-3/4 leading-4 mt-1">{subtitle}</Text>
     </>
   );
