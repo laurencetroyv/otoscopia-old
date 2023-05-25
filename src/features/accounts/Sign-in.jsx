cfe730ed16d1a1a08755555f4b067b30037c478b
laurencetroyv-hp <laurencetroyv@gmail.com>
2023-05-25 17:02:15 +0800

    refractor: code cleanup
    import all components from accounts/components folder

--- src/features/accounts/Sign-in.jsx
+++ src/features/accounts/Sign-in.jsx
@@ -1,11 +1,15 @@
-import { TouchableOpacity, View } from 'react-native';
+import { View } from 'react-native';
 import React, { useState } from 'react';
 import AnimatedLottieView from 'lottie-react-native';
-import { Button, Divider, Text, TextInput } from 'react-native-paper';
+import { Divider } from 'react-native-paper';
 import { useNavigation } from '@react-navigation/native';
 import ScreenContainer from '../../components/Screen-Container';
-import StyledTextInput from '../../components/Styled-TextInput';
 import AnimatedIllustration from '../../assets/sign-in-illustration.json';
+import Header from './components/Header';
+import StyledButton from '../../components/Styled-Button';
+import PressableText from '../../components/Pressable-Text';
+import EmailInput from './components/EmailInput';
+import PasswordInput from './components/PasswordInput';
 
 export default function SignIn() {
   const navigation = useNavigation();
@@ -18,79 +22,61 @@ export default function SignIn() {
 
   return (
     <ScreenContainer enableKeyboardAvoidingView scrollEnabled>
-      <View className="flex-1 p-4">
+      <View className="p-4 space-y-4">
         <AnimatedLottieView source={AnimatedIllustration} autoPlay loop className="w-full" />
 
-        <Divider className="mb-4" />
-
-        <View className="mb-6">
-          <Text className="font-bold text-3xl text-primary">Hello</Text>
-          <Text className="font-bold text-3xl">Login now.</Text>
-          <Text className="text-sm w-3/4 leading-4 mt-4">
-            Please fill in the form to create a new account and continue
-          </Text>
+        <View>
+          <Divider />
         </View>
 
-        <StyledTextInput
-          label="Email Address"
-          autoCapitalize="words"
-          autoComplete="email"
-          inputMode="email"
-          keyboardType="email-address"
-          onChangeText={setEmail}
-          onEndEditing={() =>
-            !email.includes('@deped.edu.ph') ? setEmailError(true) : setEmailError(false)
-          }
-          placeholder="@deped.edu.ph"
-          textContentType="emailAddress"
-          right={<TextInput.Icon icon="email" forceTextInputFocus={false} />}
-          showHelper={emailError}
-          helperMessage="Invalid email address"
-        />
+        <View>
+          <Header
+            title="Hello"
+            secondTitle="Login Now"
+            subtitle="Please fill in the form to create a new account and continue"
+          />
+        </View>
 
-        <StyledTextInput
-          label="Password"
-          autoComplete="password"
-          autoCapitalize="none"
-          inputMode="text"
-          onChangeText={setPassword}
-          onEndEditing={() =>
-            !(password.length >= 8) ? setPasswordError(true) : setPasswordError(false)
-          }
-          placeholder="P@ssw0rd"
-          secureTextEntry={passwordSecure}
-          textContentType="password"
-          right={
-            <TextInput.Icon
-              icon={passwordSecure ? 'eye' : 'eye-outline'}
-              onPress={() => setPasswordSecure(!passwordSecure)}
-              forceTextInputFocus={false}
-            />
-          }
-          showHelper={passwordError}
-          helperMessage="Invalid password"
-        />
+        <View>
+          <EmailInput
+            onChangeText={setEmail}
+            onEndEditing={() =>
+              !email.includes('@deped.edu.ph') ? setEmailError(true) : setEmailError(false)
+            }
+            showHelper={emailError}
+          />
+        </View>
 
-        <View className="space-y-4 mt-4">
-          <TouchableOpacity>
-            <Button mode="contained">
-              <Text>Sign In</Text>
-            </Button>
-          </TouchableOpacity>
+        <View>
+          <PasswordInput
+            onChangeText={setPassword}
+            onPress={() => setPasswordSecure(!passwordSecure)}
+            onEndEditing={() =>
+              !(password.length >= 8) ? setPasswordError(true) : setPasswordError(false)
+            }
+            secureTextEntry={passwordSecure}
+            showHelper={passwordError}
+          />
+        </View>
 
-          <TouchableOpacity onPress={() => navigation.navigate('Sign Up')}>
-            <Text className="text-center">
-              <Text>Don&lsquo;t have have an account?</Text>
-              <Text className="font-bold"> Register here</Text>
-            </Text>
-          </TouchableOpacity>
+        <View>
+          <StyledButton text="Sign In" />
+        </View>
 
-          <TouchableOpacity onPress={() => navigation.navigate('Forgot Password')}>
-            <Text className="text-center">
-              <Text>forgot password?</Text>
-              <Text className="font-bold"> Click here</Text>
-            </Text>
-          </TouchableOpacity>
+        <View>
+          <PressableText
+            text="Don&lsquo;t have have an account?"
+            strongText=" Register here"
+            navigate={() => navigation.navigate('Sign Up')}
+          />
+        </View>
+
+        <View>
+          <PressableText
+            text="Forgot password?"
+            strongText=" Click here"
+            navigate={() => navigation.navigate('Forgot Password')}
+          />
         </View>
       </View>
     </ScreenContainer>
