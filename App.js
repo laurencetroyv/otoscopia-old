3983f3f82551f1da7c6025aaa428ccd235ae426e
laurencetroyv-hp <laurencetroyv@gmail.com>
2023-06-01 10:35:09 +0800

    refractor: change code base from bare react-native to pure react-native

--- App.js
+++ App.js
@@ -1,16 +1,21 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
+import { StatusBar, useColorScheme } from 'react-native';
 import { Provider } from 'react-native-paper';
 import AuthenticationProvider from './src/services/Authentication-Provider';
 import Navigation from './src/infrastructure/navigation/Navigation';
+import colors from './src/infrastructure/themes/colors';
 
 export default function App() {
+  const isDarkMode = useColorScheme() === 'dark';
   return (
     <AuthenticationProvider>
       <Provider>
         <Navigation />
-        <StatusBar style="auto" />{/* eslint-disable-line react/style-prop-object */}
       </Provider>
+      <StatusBar
+        barStyle={isDarkMode ? 'light-content' : 'dark-content'}
+        backgroundColor={isDarkMode ? colors.dark.background : colors.light.background}
+      />
     </AuthenticationProvider>
   );
 }
--- babel.config.js
+++ babel.config.js
@@ -1,7 +1,4 @@
-module.exports = (api) => {
-  api.cache(true);
-  return {
-    presets: ['babel-preset-expo'],
-    plugins: ['nativewind/babel'],
-  };
+module.exports = {
+  presets: ['module:metro-react-native-babel-preset'],
+  plugins: ['nativewind/babel'],
 };
